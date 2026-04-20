b9735cd742d253b77da3c4747a9c6bc74574ab81
Eshwar954 <eshwarsaikandimalla@gmail.com>
2026-04-20 11:04:30 +0530

    first commit

---
+++ demo/src/main/java/demo_hackathon/example/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package demo_hackathon.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/demo_hackathon/example/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package demo_hackathon.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
